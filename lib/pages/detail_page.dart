ce6423393e78fe9d6c9966903a69222ce7e2384b
Duy Duc Nguyen <duyduck2809@gmail.com>
2023-09-04 11:40:40 +0700

    Update detail_page.dart

--- lib/pages/detail_page.dart
+++ lib/pages/detail_page.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_cubit/misc/colors.dart';
+import 'package:flutter_cubit/widgets/app_large_text.dart';
 
 class DetailPage extends StatefulWidget {
   const DetailPage({super.key});
@@ -14,7 +16,61 @@ class _DetailPageState extends State<DetailPage> {
       body: Container(
         width: double.maxFinite,
         height: double.maxFinite,
-        child: Stack(),
+        child: Stack(
+          children: [
+            Positioned(
+                left: 0,
+                right: 0,
+                child: Container(
+                  width: double.maxFinite,
+                  height: 350,
+                  decoration: BoxDecoration(
+                    image: DecorationImage(
+                        image: AssetImage('img/mountain.jpeg'),
+                        fit: BoxFit.cover),
+                  ),
+                )),
+            Positioned(
+                top: 50,
+                left: 20,
+                child: Row(
+                  children: [
+                    IconButton(
+                      onPressed: () {},
+                      icon: Icon(Icons.menu),
+                      color: Colors.white,
+                    )
+                  ],
+                )),
+            Positioned(
+                top: 320,
+                child: Container(
+                  padding: const EdgeInsets.only(left: 20, right: 20, top: 30),
+                  color: Colors.red,
+                  width: MediaQuery.of(context).size.width,
+                  height: 500,
+                  decoration: BoxDecoration(
+                      color: Colors.white,
+                      borderRadius: BorderRadius.only(
+                          topLeft: Radius.circular(30),
+                          topRight: Radius.circular(30))),
+                  child: Column(
+                    children: [
+                      Row(children: [
+                        AppLargeText(
+                          text: 'Yosemite',
+                          color: Colors.black.withOpacity(0.8),
+                        ),
+                        AppLargeText(
+                          text: '\$ 250',
+                          color: AppColors.mainColor,
+                        )
+                      ]),
+                    ],
+                  ),
+                ))
+          ],
+        ),
       ),
     );
   }
