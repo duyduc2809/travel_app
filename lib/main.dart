af5c717e705eb0280545b9a4e48c8e4af782c0de
Duy Duc Nguyen <duyduck2809@gmail.com>
2023-09-05 15:40:02 +0700

    update first cubit

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:flutter_cubit/cubit/app_cubit_logics.dart';
+import 'package:flutter_cubit/cubit/app_cubits.dart';
 import 'package:flutter_cubit/pages/detail_page.dart';
 import 'package:flutter_cubit/pages/navpages/main_page.dart';
 import 'package:flutter_cubit/pages/welcome_page.dart';
@@ -19,6 +22,9 @@ class MyApp extends StatelessWidget {
         theme: ThemeData(
           primarySwatch: Colors.blue,
         ),
-        home: DetailPage());
+        home: BlocProvider<AppCubits>(
+          create: (context) => AppCubits(),
+          child: AppCubitLogics(),
+        ));
   }
 }
